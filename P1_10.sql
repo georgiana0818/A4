9a7debd9aff9b3a5925a1ab3eed39714bccc7c10
georgiana0818 <georgiana0818@gmail.com>
2018-11-14 02:01:30 -0500

    update

--- P1_10.sql
+++ P1_10.sql
@@ -5,8 +5,10 @@ all the same services as Jason Allison subscribes to
 but possibly others as well. Jason Allison rents the line with portID=2.
 */
 
-select name from subscribers, (select  count(*) as numSubscribe from service_subscribers where line = 2), (select line, count(*) AS sevNum from service_subscribers
-where service in (select service from service_subscribers where line = 2) group by line) where numSubscribe = sevNum AND line = portid;
+select name from subscribers, (select  count(*) as numSubscribe from service_subscribers where line = 2), 
+(select line, count(*) AS sevNum from service_subscribers
+where service in (select service from service_subscribers where line = 2) group by line)
+where numSubscribe = sevNum AND line = portid;
 
 
 /*
